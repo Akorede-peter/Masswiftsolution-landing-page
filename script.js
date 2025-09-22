e4f6b8efe156ae43f75e41ffa241c0f58ac03229
kenny-svg <kennyfagbenro44@gmail.com>
2025-09-22 19:07:38 +0100

    Add landing page files

---
+++ script.js
@@ -0,0 +1,77 @@
+// const menuButton = document.getElementById("menuButton");
+//  const menu = document.getElementById("menu");
+//  menuButton.addEventListener("click", () => { 
+//   menu.classList.toggle("show"); });
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.getElementById("menuButton");
+  const menu = document.getElementById("menu");
+  const links = menu.querySelectorAll("a");
+
+  // Toggle open/close
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("show");
+  });
+
+  // Close when clicking a link
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("show");
+    });
+  });
+});
+function scrollToSection(register) {
+  const section = document.getElementById(register);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+console.log("Script loaded!");
+/*
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+  const triggerHeight = window.innerHeight / 2; // halfway down the screen
+
+  if (window.scrollY > triggerHeight) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
+const firstName ='Akorede';
+const age = 24;
+const job = 'Developer';
+console.log(typeof age);
+const age = 9;
+const isOldEnough = age >= 18;
+if(isOldEnough){
+    console.log(" Peter can start driving 🚗🚗");
+}else{
+    const yearsLeft = 18 - age;
+    console.log(`Peter is too young. Wait another ${yearsLeft} years`);
+}
+
+const birthYear = 2025;
+let century;
+
+if (birthYear <= 2000){
+     century = 20;
+}else{
+     century = 21;
+}
+console.log(century);
+console.log(Boolean(1));
+
+const favorite = Number(prompt("What is your favourite number"));
+
+if ( favorite === 23) {
+  console.log(" Cool! 23 is an amazing number");
+} else if ( favorite === 7){
+  console.log(" 7 is also a cool Number")
+}else {
+  console.log(" Number is not 23 or 7")
+}
+
+if ( favorite !== 23) console.log("Cool number, why not 23");
+*/
